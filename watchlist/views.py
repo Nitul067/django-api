e819fbeaa34295733a30a22ebba7c50f54f00c79
Nitul067 <85896310+Nitul067@users.noreply.github.com>
2023-06-28 23:10:45 +0530

    project setup

---
+++ watchlist/views.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+from .models import Movie
+
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies': list(movies.values())
+    }
+    return JsonResponse(context)
+
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    context = {
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active,
+    }
+    return JsonResponse(context)
